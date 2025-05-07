c1b192dd427b5d05ac8fb4472ae70d248efc0f0c
Abanoub Mokhles <mokhles.abanoub1@gmail.com>
2025-05-07 12:51:47 +0300

    add basic components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__heading">Branch Distribution Tool</h1>
+    </header>
+  );
+};
+
+export default Header;
